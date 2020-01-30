1f463da247c156e4209d2a9b3c89a7e5099dd17c
Gabi <gabrielaradu.developer@gmail.com>
2020-01-30 21:43:31 +0200

    Rename controller to better represent what it does

--- src/controllers/todoList_controller.js
+++ src/controllers/todoList_controller.js
@@ -1,15 +1,14 @@
 import {Controller} from "stimulus";
 
-let Datastore = require('nedb');
-
 export default class extends Controller {
     static targets = ['item'];
-    allItems = new Datastore();
 
     initialize() {
         this.allItems = {
             getAllData() {
-                return ['This is your first long task description', 'Create a todo app with Stimulus Js and set a deadline for this work.', 'Share the project publicly on Github']
+                return [{_id: 'id11', name: 'This is your first long task description'},
+                    {_id: 'id12', name: 'Create a todo app with Stimulus Js and set a deadline for this work.'},
+                    {_id: 'id13', name: 'Share the project publicly on Github'}]
             }
         };
     }
@@ -18,7 +17,7 @@ export default class extends Controller {
         this.allItems.getAllData().map(item => {
                 let li = document.createElement('li');
                 li.classList.add('flex', 'm-5', 'p-2', 'items-start', 'content-start', 'bg-red-100');
-                li.innerHTML += item;
+                li.innerHTML += item.name;
                 this.itemTarget.appendChild(li);
             }
         );
