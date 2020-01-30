273b9e89f0b8071967524ac40de00f570798ddba
Gabi <gabrielaradu.developer@gmail.com>
2020-01-30 21:53:29 +0200

    Show items from storage. Does not refresh the page. WIP

--- src/controllers/todoList_controller.js
+++ src/controllers/todoList_controller.js
@@ -1,23 +1,18 @@
 import {Controller} from "stimulus";
+import {TODO_STORAGE_KEY} from "../common";
 
 export default class extends Controller {
     static targets = ['item'];
 
-    initialize() {
-        this.allItems = {
-            getAllData() {
-                return [{_id: 'id11', name: 'This is your first long task description'},
-                    {_id: 'id12', name: 'Create a todo app with Stimulus Js and set a deadline for this work.'},
-                    {_id: 'id13', name: 'Share the project publicly on Github'}]
-            }
-        };
+    initialize(key) {
+        this.allItems = localStorage && JSON.parse(localStorage.getItem(TODO_STORAGE_KEY));
     }
 
     connect() {
-        this.allItems.getAllData().map(item => {
+        this.allItems.map(item => {
                 let li = document.createElement('li');
                 li.classList.add('flex', 'm-5', 'p-2', 'items-start', 'content-start', 'bg-red-100');
-                li.innerHTML += item.name;
+                li.innerHTML += item;
                 this.itemTarget.appendChild(li);
             }
         );
