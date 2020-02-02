b13671b7f7bcc5b100ca4c17c2ba2c71fc179178
Gabi <gabrielaradu.developer@gmail.com>
2020-02-02 22:56:05 +0200

    Show all existing todos and move add logic to a single controller.

--- src/controllers/todoList_controller.js
+++ src/controllers/todoList_controller.js
@@ -1,20 +1,52 @@
 import {Controller} from "stimulus";
-import {TODO_STORAGE_KEY} from "../common";
+import {EMPTY_STRING, TODO_STORAGE_KEY} from "../common";
 
 export default class extends Controller {
-    static targets = ['item'];
-
-    initialize(key) {
-        this.allItems = localStorage && JSON.parse(localStorage.getItem(TODO_STORAGE_KEY));
-    }
+    static targets = ['name', 'show', 'template'];
+    itemsToAdd = [];
 
     connect() {
-        this.allItems.map(item => {
-                let li = document.createElement('li');
-                li.classList.add('flex', 'm-5', 'p-2', 'items-start', 'content-start', 'bg-red-100');
-                li.innerHTML += item;
-                this.itemTarget.appendChild(li);
-            }
-        );
+        this.loadTodoList();
+    }
+
+    appendTodo(todoValue) {
+        let todo = document.importNode(this.templateTarget.content, true);
+        todo.querySelector('li').dataset.value = todoValue;
+        this.showTarget.appendChild(todo);
+    }
+
+    loadTodoList() {
+        let data = JSON.parse(localStorage.getItem(TODO_STORAGE_KEY));
+        if (data) {
+            let _this = this;
+            data.forEach(function (todo) {
+                _this.appendTodo(todo);
+            })
+        }
+    }
+
+    add() {
+        let todoToAdd = this.nameTarget.value;
+        this.itemsToAdd.push(todoToAdd);
+
+        let existingData = JSON.parse(localStorage.getItem(TODO_STORAGE_KEY) || "[]");
+        if (existingData === []) {
+            localStorage && localStorage.setItem(TODO_STORAGE_KEY, JSON.stringify(this.itemsToAdd));
+        } else {
+            this.itemsToAdd.push(...existingData);
+            localStorage.setItem(TODO_STORAGE_KEY, JSON.stringify(this.itemsToAdd));
+        }
+        this.itemsToAdd = [];
+        this.clearInput();
+
+        this.appendTodo(todoToAdd);
+    }
+
+    clearInput() {
+        this.nameTarget.value = EMPTY_STRING;
+    }
+
+    get name() {
+        return this.nameTarget.value;
     }
 }
